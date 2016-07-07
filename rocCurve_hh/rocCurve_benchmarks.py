ec477ddbf6e40c968e6d2563d7e51f9a8b0ec1aa
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-07-07 19:08:04 +0200

    Update location in rocCorve tools

--- rocCurve_hh/rocCurve_benchmarks.py
+++ rocCurve_hh/rocCurve_benchmarks.py
@@ -8,7 +8,7 @@ R.gROOT.SetBatch()
 # Tool to produce ROC curves for all the benchmarks used in the training, and for each BDT
 # Usage : python ... outRocPlotDir
 
-inFileDir = "/home/fynu/swertz/scratch/CMSSW_7_6_3_patch2/src/cp3_llbb/HHTools/condor/160622_allGood_scaleNoNorm_0/condor/output/"
+inFileDir = "/home/fynu/sbrochet/scratch/Framework/CMSSW_7_6_5/src/cp3_llbb/HHTools/histFactory_hh/160706_all_newjer_newtraining_fixedcluster7_0/condor/output/"
 
 outFileDir = "rocCurve_" + sys.argv[1]
 if not os.path.isdir(outFileDir):
@@ -16,28 +16,29 @@ if not os.path.isdir(outFileDir):
 print "Will write ROC curves in %s "%(outFileDir)
 
 backgrounds = [ # will take all background to compute background efficiency
-    inFileDir + "TT_FH_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "TT_FL_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "TT_SL_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg_Fall15MiniAODv2_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "ST_tW_top_5f_inclusiveDecays_13TeV-powheg_Fall15MiniAODv2_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_extended_ext0_plus_ext1_plus_ext3_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
-    inFileDir + "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_extended_ext0_plus_ext1_plus_ext4_v0.1.2+76X_HHAnalysis_2016-05-02.v0_histos.root",
+    inFileDir + "TT_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    #inFileDir + "TT_FH_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    #inFileDir + "TT_FL_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    #inFileDir + "TT_SL_TuneCUETP8M1_13TeV-powheg-pythia8_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    inFileDir + "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    inFileDir + "ST_tW_top_5f_inclusiveDecays_13TeV-powheg_Fall15MiniAODv2_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    inFileDir + "DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_extended_ext0_plus_ext1_plus_ext3_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
+    inFileDir + "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_extended_ext0_plus_ext1_plus_ext4_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root",
     ]
 
 variablesForRocCurves = [] #on each canvas, draw one rocCurve per variable
-nodes_mva = ["SM", "2", "5", "6", "12", "all"]
-date = "2016_06_10"
+nodes_mva = ["SM", "2", "5", "6", "7", "12"]
+date = "2016_07_05"
 suffix = "VS_TT_DYHTonly_tW_8var"
 bdtTemplate = "MVA_DATE_BDT_NODE_SUFFIX_All_hh_llmetjj_HWWleptons_btagM_csv_mll_cut"
 
 # will make on canvas per signal
 nodes_sig = nodes_mva
-nodes_sig.remove("all")
+#nodes_sig.remove("all")
 
 gStyle()
 for node in nodes_sig:
-    signal = inFileDir + "GluGluToHHTo2B2VTo2L2Nu_node_{}_13TeV-madgraph_v0.1.3+76X_HHAnalysis_2016-06-03.v0_histos.root".format(node)
+    signal = inFileDir + "GluGluToHHTo2B2VTo2L2Nu_node_{}_13TeV-madgraph_v0.1.4+76X_HHAnalysis_2016-06-03.v0_histos.root".format(node)
     sigFile = R.TFile(signal)
     
     bkgFiles = []
@@ -56,6 +57,7 @@ for node in nodes_sig:
     for mva in nodes_mva:
         
         var = bdtTemplate.replace("DATE", date).replace("NODE", mva).replace("SUFFIX", suffix)
+        print("Working on %r" % var)
         
         sigTH1[var] = sigFile.Get(var)
         bkgTH1[var] = bkgFiles[0].Get(var)
