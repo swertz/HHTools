2d351bf4921ff6d3d4bacfcad7eabbd37af468cc
Sebastien Wertz <sebastien.wertz@uclouvain.be>
2017-03-15 22:10:10 +0100

    Tweaks for postfit

--- plotIt_hh/postFit/moveHistos.sh
+++ plotIt_hh/postFit/moveHistos.sh
@@ -11,12 +11,12 @@ for c in MuMu MuEl ElEl; do
         done
     done
     
-    for m in 1p00_1p00 2p40_2p50 m20p00_0p50; do
+    for m in 1p00_1p00 5p00_2p50 m20p00_0p50; do
         echo "Doing $m"
         rootcp --replace GluGluToHHTo2B2VTo2L2Nu_point_${m}_13TeV-madgraph_v5.0.1+80X_HHAnalysis_2017-03-01.v0_histos.root:jj_pt_${c}_hh_llmetjj_HWWleptons_btagM_cmva_mll_cut               ggHH_${m}_postfit_histos.root:ptjj_${c}
         rootcp --replace GluGluToHHTo2B2VTo2L2Nu_point_${m}_13TeV-madgraph_v5.0.1+80X_HHAnalysis_2017-03-01.v0_histos.root:jj_M_${c}_hh_llmetjj_HWWleptons_btagM_cmva_mll_cut                ggHH_${m}_postfit_histos.root:mjj_${c}
     
-        for m2 in 1p00_1p00 2p40_2p50 m20p00_0p50; do
+        for m2 in 1p00_1p00 5p00_2p50 m20p00_0p50; do
             rootcp --replace GluGluToHHTo2B2VTo2L2Nu_point_${m}_13TeV-madgraph_v5.0.1+80X_HHAnalysis_2017-03-01.v0_histos.root:NN_nonresonant_point_${m2}_${c}_hh_llmetjj_HWWleptons_btagM_cmva_mll_cut    ggHH_${m}_postfit_histos.root:NN_${m2}_${c}
             rootcp --replace GluGluToHHTo2B2VTo2L2Nu_point_${m}_13TeV-madgraph_v5.0.1+80X_HHAnalysis_2017-03-01.v0_histos.root:flatDrop_mjj_vs_NN_nonresonant_point_${m2}_${c}_hh_llmetjj_HWWleptons_btagM_cmva_mll_cut    ggHH_${m}_postfit_histos.root:mjj_vs_NN_${m2}_${c}
         done
--- plotIt_hh/postFit/producePostFitPlots.sh
+++ plotIt_hh/postFit/producePostFitPlots.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-input=170314_postFitShapes/nonresonant_M400
-output=plots_all_2017-03-14_postfit
+#input="170315_postFitShapes/M900/plotIt/"
+input="170315_postFitShapes/1p00_1p00/plotIt/"
+output="plots_all_2017-03-15_postfit"
 
 sed -i "s|shapes|${input}|g" centralConfig_shapes_postfit.yml
 
 # MuMu
-../../plotIt/plotIt hh_plotter_all_shapes_postfit.yml -o ${output}
+../../../plotIt/plotIt -o ${output} -- hh_plotter_all_shapes_postfit.yml
 
 # MuEl
 sed -i 's/MuMu/MuEl/g' postfitPlots.yml
-sed -i 's/#mumu channel/#mue + e#mu channels/g' postfitPlots.yml
-../../plotIt/plotIt hh_plotter_all_shapes_postfit.yml -o ${output}
+sed -i 's/#mu#mu channel/#mue + e#mu channels/g' postfitPlots.yml
+../../../plotIt/plotIt -o ${output} -- hh_plotter_all_shapes_postfit.yml
 
 # ElEl
 sed -i 's/MuEl/ElEl/g' postfitPlots.yml
 sed -i 's/#mue + e#mu channels/ee channel/g' postfitPlots.yml
-../../plotIt/plotIt hh_plotter_all_shapes_postfit.yml -o ${output}
+../../../plotIt/plotIt -o ${output} -- hh_plotter_all_shapes_postfit.yml
 
 # back to MuMu
 sed -i 's/ElEl/MuMu/g' postfitPlots.yml
